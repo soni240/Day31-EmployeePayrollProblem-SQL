26c4ebf1818666a028cb80c3f4e4f52b02400260
soni240 <s.fegade21@gmail.com>
2022-07-22 12:02:33 +0530

    [Gunavanti]Add:Rename Salary

--- UC9-taxincomenetpay.sql
+++ UC9-taxincomenetpay.sql
@@ -1,3 +1,4 @@
+
 --UC-1 creating database
 create database EmployeeServices;
 
@@ -64,3 +65,21 @@ where name ='Tanvi';
 Update employee_payroll 
 set EmployeePhoneNumber='8715605050',Address='Bareilly,UP'
 where name ='John';
+
+
+---- UC 9: Rename Salary to Basic Pay and Add Deduction,Taxable pay, Income Pay , Netpay 
+
+EXEC sp_RENAME 'EmpoyeePayroll.Basic Pay' , 'BasicPay', 'COLUMN'
+Alter table employee_payroll
+add Deduction float,TaxablePay float, IncomeTax float,NetPay float;
+Update EmployeePayroll 
+set Deduction=1000
+where Gender='F';
+Update EmployeePayroll 
+set Deduction=2000
+where Gender='M';
+update EmployeePayroll
+set NetPay=(BasicPay - Deduction)
+update EmployeePayroll
+set TaxablePay=0,IncomeTax=0
+select * from EmpoyeePayroll;
